ff2d56da1ba46ba3e4b758be4c931e80188beff8
Alex0494 <alessio.cozzolino@outlook.it>
2016-01-15 10:22:41 +0100

    14-01-16

--- app/public/js/controllers/ApplicationController.js
+++ app/public/js/controllers/ApplicationController.js
@@ -4,21 +4,22 @@ angular.module('esame')
 	$scope.user = null;
 
 	$scope.getPictures = function() {		
-		$facebook.api('/me/photos').then(function(resp){
+		$facebook.api('/me?fields=photos').then(function(resp){
 			if(resp) {
-				$scope.pictures = resp.data;				
+				$scope.pictures = resp.data;
+				console.log(resp);				
 			}else{
 				console.log("error");
 			}
 		});
 	};
+
 	//init function
 	$scope.$on('$viewContentLoaded', function(){
 		if(Session.logged) {
-			$facebook.api('/me').then(function(resp){
-				$scope.user = {name: resp.name};
-			});
-			$scope.getPictures();
+			$facebook.api('/me?fields=id,name,picture').then(function(resp){
+				$scope.user = resp;				
+			});		
 		};   			
-  	});
+	});
 }]);
