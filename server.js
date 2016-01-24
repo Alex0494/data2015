a84ffd6ffa659a20d1263fe706ffa0a3710decde
Alex0494 <alessio.cozzolino@outlook.it>
2016-01-24 20:58:51 +0100

    edited structure for heroku deployment

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 (function(){
 
-var express = require('express');
-var app = express();
+	var express = require('express');
+	var app = express();
+	var port = process.env.PORT || 8080;
+	app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'));
+	app.get('/', function(req, resp) {
+		resp.sendFile(__dirname + '/public/index.html');
+	});
 
-app.get('/', function(req, resp) {
-	resp.sendFile(__dirname + '/public/index.html');
-	console.log("welcome!!!")
-});
-
-app.post('/pictures', function(req, resp){
-
-});
-
-app.listen(3000);
-console.log("listening on port 3000");
+	app.listen(port, function(){
+		console.log("listening on port:" + port);
+	});
 
 })();
